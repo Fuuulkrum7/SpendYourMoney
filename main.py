69085b4ef14d22e0a0e2fae7965ace3a2546f968
Fuuulkrum7 <ilka747428@gmail.com>
2023-01-22 23:53:20 +0300

    created db structure

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-import os
+import asyncio
 from datetime import timedelta
 
-from tinkoff.invest import CandleInterval, Client
+from tinkoff.invest import CandleInterval, AsyncClient
 from tinkoff.invest.utils import now
 
 TOKEN = "t.0GnEOB1p5ODjob-f4qhnvbf2xgH1Up6ORFTOfiVKjd7EP4g_SkM8lQWX4Cins9fHNnb3oBqS4dzwQGBt1t7XVA"
 
 
-def main():
-    with Client(TOKEN) as client:
-        for candle in client.get_all_candles(
-            figi="BBG004731032",
-            from_=now() - timedelta(minutes=60),
-            interval=CandleInterval.CANDLE_INTERVAL_HOUR,
+async def main():
+    async with AsyncClient(TOKEN) as client:
+        async for candle in client.get_all_candles(
+            figi="BBG004730N88",
+            from_=now() - timedelta(minutes=1),
+            interval=CandleInterval.CANDLE_INTERVAL_1_MIN,
         ):
             print(candle)
 
-    return 0
 
+"""
+def main():
+    with Client(TOKEN) as client:
+        r = client.instruments.find_instrument(query="газ")
+        for i in r.instruments:
+            if i.instrument_type == "bond":
+                print(i)
+"""
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
