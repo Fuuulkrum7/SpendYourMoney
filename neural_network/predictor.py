f8cf824fd78c1162d458856f3a10101ffa68ee88
Fuuulkrum7 <ilka747428@gmail.com>
2023-04-11 16:05:31 +0300

    add keras model

---
+++ neural_network/predictor.py
@@ -0,0 +1,78 @@
+from datetime import timedelta
+
+from PyQt5.QtCore import QThread, pyqtSignal
+from keras import models
+from tinkoff.invest import CandleInterval
+from tinkoff.invest.utils import now
+
+from api_requests.get_security_history import GetSecurityHistory
+from neural_network.data_preparation import normalize_data
+from securities.securities import Stock
+
+
+class PredictCourse(QThread):
+    # Интересно, что же это такое... Даже не знаю
+    status_code: int = 200
+    stock: Stock
+    data_downloaded = pyqtSignal(object)
+    result: list
+    history: list
+
+    def __init__(self, stock: Stock, on_finish, token):
+        super().__init__()
+        self.securities_thread = None
+        self.stock = stock
+        self.data_downloaded.connect(on_finish)
+        self.__token = token
+
+    def run(self) -> None:
+        self.load_history()
+        self.securities_thread.wait()
+
+        if self.status_code != 200:
+            return
+
+        try:
+            model = models.load_model("model_best.keras")
+
+            parsed = [i.get_as_dict() for i in self.history]
+            for i in parsed:
+                i.pop("security_id")
+
+            security = self.stock.get_as_dict()
+            security.update(self.stock.get_as_dict_security())
+
+            security["history"] = parsed
+
+            norm = normalize_data(security)
+
+            self.result = list(model.predict(norm))
+
+            self.data_downloaded.emit((self.status_code, self.result))
+        except Exception as e:
+            print(e)
+            self.status_code = 500
+
+    def on_load(self, result):
+        code, data = result
+        if code != 200:
+            self.status_code = 400
+            return
+
+        if len(data) < 120:
+            self.status_code = 300
+            return
+
+        self.history = data
+
+    def load_history(self):
+        self.securities_thread = GetSecurityHistory(
+                info=self.stock.info,
+                _from=now() - timedelta(days=1001),
+                to=now() - timedelta(days=1),
+                interval=CandleInterval.CANDLE_INTERVAL_DAY,
+                token=self.__token,
+                on_finish=self.on_load
+            )
+
+        self.securities_thread.start()
---
+++ neural_network/stocks_course.py
@@ -0,0 +1,107 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras import Sequential, models
+from keras.layers import Dense, GRU
+from keras.optimizers import RMSprop
+
+from data_preparation import *
+
+
+data_test = get_json("parsed_test_data.json")
+data_main = get_json("parsed_data.json")
+
+codes = get_json("sector_codes.json")
+
+test_his = normalize_data(data_test)[0]
+sec_history = normalize_data(data_main)[0]
+
+spliter = 1024 - 30 - 1
+
+train_data_h = sec_history[:]
+
+train_target = train_data_h[:, spliter:, :1]
+train_data_h = train_data_h[:, spliter-120:spliter]
+
+check_data_h = sec_history[1000:]
+
+check_target = check_data_h[:, spliter:, :1]
+check_data_h = check_data_h[:, spliter-120:spliter]
+
+test_target = test_his[:, spliter:, :1]
+test_his = test_his[:, spliter-120:spliter]
+
+graph = np.array(test_target)
+
+# mark as growing or falling
+test_target = np.array([is_growing(i) for i in test_target])
+
+train_target = np.array([is_growing(i) for i in train_target])
+check_target = np.array([is_growing(i) for i in check_target])
+
+# model_test = models.load_model("model.keras")
+# print(model_test.summary())
+#
+# test_loss, test_acc = model_test.evaluate(check_data_h, check_target)
+# print('test_acc:', test_acc)
+#
+# a = model_test.predict(test_his)
+#
+# for i in range(len(a)):
+#     plt.plot(list(range(1, len(graph[i]) + 1)), graph[i])
+#     plt.title(f"{a[i]}, {test_target[i]}")
+#     plt.show()
+#     plt.clf()
+
+
+model = Sequential()
+
+model.add(
+    GRU(
+        32,
+        input_shape=train_data_h[0].shape,
+        activation='relu',
+        return_sequences=True
+    )
+)
+
+model.add(GRU(
+    64,
+    activation='relu'
+))
+
+model.add(Dense(3, activation='sigmoid'))
+
+model.compile(
+    loss='mse',
+    metrics=['mae'],
+    optimizer=RMSprop()
+)
+
+history = model.fit(
+    train_data_h,
+    train_target,
+    epochs=1000,
+    batch_size=64,
+    verbose=1,
+)
+
+test_loss, test_acc = model.evaluate(check_data_h, check_target)
+print('test_acc:', test_acc)
+
+mae_history = history.history['mae']
+
+plt.plot(range(1, len(mae_history) + 1), mae_history)
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+plt.show()
+plt.clf()
+
+model.save('model.keras')
+
+a = model.predict(test_his)
+
+for i in range(len(a)):
+    plt.plot(list(range(1, len(graph[i]) + 1)), graph[i])
+    plt.title(f"{a[i]}, {test_target[i]}")
+    plt.show()
+    plt.clf()
