92e30d242c7dae6084dfe2717571090517d9c6dc
Fuuulkrum7 <ilka747428@gmail.com>
2023-04-27 17:00:11 +0300

    fix in offline application work

--- neural_network/predictor.py
+++ neural_network/predictor.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import timedelta, timezone
 
 import numpy as np
 from PyQt5.QtCore import QThread, pyqtSignal
@@ -12,11 +12,18 @@ from securities.securities import Stock
 
 
 class PredictCourse(QThread):
+    """
+    This class is used for making prediction of securities course using neural
+    network. In fact, it is not perfect in such predictions, but it is not bad.
+    Status-codes:
+    200 - everything is ok
+    250 - data can be not up-to-date, data is not actual
+    """
     # Интересно, что же это такое... Даже не знаю
     status_code: int = 200
     stock: Stock
     data_downloaded = pyqtSignal(object)
-    result: list
+    result: list = []
     history: list
 
     def __init__(self, stock: Stock, on_finish, token):
@@ -25,42 +32,45 @@ class PredictCourse(QThread):
         self.stock = stock
         self.data_downloaded.connect(on_finish)
         self.__token = token
+        self.to = now() - timedelta(days=1)
+        self.to = self.to.replace(tzinfo=timezone.utc)
 
     def run(self) -> None:
         self.load_history()
         self.securities_thread.wait()
 
-        if self.status_code != 200:
-            return
+        if self.status_code < 300:
+            try:
+                model = load_model("neural_network/model_best.keras")
 
-        try:
-            model = load_model("neural_network/model_best.keras")
+                parsed = [i.get_as_dict() for i in self.history]
+                for i in parsed:
+                    i.pop("security_id")
 
-            parsed = [i.get_as_dict() for i in self.history]
-            for i in parsed:
-                i.pop("security_id")
+                security = self.stock.get_as_dict()
+                security.update(self.stock.get_as_dict_security())
 
-            security = self.stock.get_as_dict()
-            security.update(self.stock.get_as_dict_security())
+                security["history"] = parsed
 
-            security["history"] = parsed
+                norm = normalize_data({"data": security})[0]
 
-            norm = normalize_data({"data": security})[0]
+                self.result = model.predict(norm[:, -120:])[0] * 100
 
-            self.result = model.predict(norm[:, -120:])[0] * 100
+                self.result = list(np.around(self.result, decimals=5))
+            except Exception as e:
+                print(e)
+                self.status_code = 500
 
-            self.result = list(np.around(self.result, decimals=5))
-
-            self.data_downloaded.emit((self.status_code, self.result))
-        except Exception as e:
-            print(e)
-            self.status_code = 500
+        self.data_downloaded.emit((self.status_code, self.result))
 
     def on_load(self, result):
         code, data = result
-        if code != 200:
+
+        if code == 500 or code == 300:
             self.status_code = 400
             return
+        if code == 400 and data[-1].info_time.date() < self.to.date():
+            self.status_code = 250
 
         if len(data) < 120:
             self.status_code = 300
@@ -72,7 +82,7 @@ class PredictCourse(QThread):
         self.securities_thread = GetSecurityHistory(
                 info=self.stock.info,
                 _from=now() - timedelta(days=1001),
-                to=now() - timedelta(days=1),
+                to=self.to,
                 interval=CandleInterval.CANDLE_INTERVAL_DAY,
                 token=self.__token,
                 on_finish=self.on_load
