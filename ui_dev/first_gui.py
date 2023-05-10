60ccb91a06c1f1ce5ed5915382a3125b02f256fe
KrasavinDaniil <krasavin.danila@yandex.ru>
2023-05-10 15:57:30 +0300

    add tabs

--- ui_dev/first_gui.py
+++ ui_dev/first_gui.py
@@ -509,33 +509,6 @@ class CreateWindow:
         self.reg.show()
 
 
-class TableWidget(QWidget):
-    def __init__(self, parent):
-        super(QWidget, self).__init__(parent)
-        self.layout = QVBoxLayout(self)
-
-        # Initialize tab screen
-        self.tabs = QTabWidget()
-        self.tab1 = QWidget()
-        self.tab2 = QWidget()
-        self.tabs.resize(300, 200)
-
-        # Add tabs
-        self.tabs.addTab(self.tab1, "Tab 1")
-        self.tabs.addTab(self.tab2, "Tab 2")
-
-        # Create first tab
-        # self.tab1.layout = QVBoxLayout(self)
-        # self.pushButton1 = QPushButton("PyQt5 button")
-        # self.tab1.layout.addWidget(self.pushButton1)
-        # self.tab1.setLayout(self.tab1.layout)
-
-
-        # Add tabs to widget
-        self.layout.addWidget(self.tabs)
-        self.setLayout(self.layout)
-
-
 class SecurityWindow(QMainWindow):
     get_securities_thread: GetSecurity = None
 
