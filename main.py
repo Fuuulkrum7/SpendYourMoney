dc4c2f68c8d90bc50d8b43b91e4d23a7de2f0bb6
Fuuulkrum7 <ilka747428@gmail.com>
2023-05-20 01:43:27 +0300

    fix in dark mode

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from ui_dev.first_gui import CreateWindow
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    with open("ui_dev\styles.css", "r") as style:
+    with open("ui_dev/styles.css", "r") as style:
         app.setStyleSheet(style.read())
 
     wndw = CreateWindow(app)
