a7d45530cdecf5b967df1d6231da057dc09e439b
Vadim <vadim.elkin.april@yandex.ru>
2023-05-29 21:23:30 +0300

    update rsi

--- prediction/rsi.py
+++ prediction/rsi.py
@@ -1,10 +1,18 @@
 import datetime
+import modulefinder
 
 from api_requests.get_security_history import GetSecurityHistory
 from PyQt5.QtCore import QThread as Thread, pyqtSignal
 from tinkoff.invest import CandleInterval
 
 from securities.securities import SecurityInfo
+
+delta: list = [datetime.timedelta(minutes=1), datetime.timedelta(minutes=5),
+               datetime.timedelta(minutes=15), datetime.timedelta(hours=1),
+               datetime.timedelta(days=1), datetime.timedelta(minutes=2),
+               datetime.timedelta(minutes=3), datetime.timedelta(minutes=10),
+               datetime.timedelta(minutes=30), datetime.timedelta(hours=2),
+               datetime.timedelta(hours=4), datetime.timedelta(weeks=1)]
 
 
 class RSI(Thread):
@@ -16,15 +24,13 @@ class RSI(Thread):
     info: SecurityInfo
     candle_interval: CandleInterval
     data_downloaded = pyqtSignal(object)
-    sensitive: float
 
     def __init__(self, num_candl: int, token, start_date: datetime,
                  end_date: datetime, info: SecurityInfo, on_finish,
-                 rsi_step: int = 14, sensitive: float = 2.0,
+                 rsi_step: int = 14,
                  candle_interval: CandleInterval =
                  CandleInterval.CANDLE_INTERVAL_DAY):
         super().__init__()
-        self.sensitive = sensitive
         self.rsi_step = rsi_step
         self.num_candl = num_candl
         self.__token = token
@@ -42,50 +48,46 @@ class RSI(Thread):
 
     def get_rsi_in_point(self):
         output: list = []
+        if len(self.get_sec.history) - self.rsi_step < self.num_candl:
+            self.num_candl = len(self.get_sec.history) - self.rsi_step
         if self.status_code < 300:
             try:
                 target_candle = 1
-                while target_candle < self.num_candl:
-                    up_sum = 0
-                    up_num: int = 0
-                    down_sum = 0
-                    down_num: int = 0
+                while target_candle <= self.num_candl:
+                    gain: list = []
+                    lost: list = []
                     prev_candle = 0
-                    skiped_candle = 0
+                    skipped_candle = 0
                     rsi_num = 0
                     for candle in self.get_sec.history:
-                        if skiped_candle < target_candle - 1:
-                            skiped_candle += 1
+                        if skipped_candle < target_candle - 1:
+                            skipped_candle += 1
                         else:
                             if rsi_num < self.rsi_step:
                                 if prev_candle == 0:
                                     prev_candle = candle.price
                                 else:
-                                    if prev_candle >= candle.price:
-                                        down_sum = down_sum + 100 * \
-                                                   (1 - candle.price /
-                                                    prev_candle)
-                                        down_num = down_num + 1
+                                    val = ((candle.price / prev_candle) - 1) \
+                                          * 100
+                                    if val > 0:
+                                        gain.append(val)
+                                        lost.append(0)
                                     else:
-                                        up_sum = up_sum + 100 * \
-                                                 (1 - prev_candle /
-                                                  candle.price)
-                                        up_num = up_num + 1
+                                        lost.append(-val)
+                                        gain.append(0)
+                                    prev_candle = candle.price
                                 rsi_num += 1
                             else:
                                 break
-                    if up_num == 0:
-                        up_sum = down_sum
-                        up_num = down_num
-                    if down_num == 0:
-                        down_sum = up_sum
-                        down_num = up_num
-                    output.append(100 - 100 / (1 +
-                                               ((up_sum / up_num)
-                                                /(down_sum / down_num))))
+                    if sum(gain) == 0:
+                        gain.append(1)
+                    if sum(lost) == 0:
+                        lost.append(1)
+                    output.append((100 / (1 +
+                                                ((sum(gain) / len(gain))
+                                                 / (sum(lost) / len(lost))))))
                     target_candle += 1
             except Exception as e:
-                print(e)
                 self.status_code = 500
         self.data_downloaded.emit((self.status_code, output))
 
